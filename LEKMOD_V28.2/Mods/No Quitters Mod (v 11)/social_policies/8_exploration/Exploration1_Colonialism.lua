47dc5528e6e52b34d8e4640745e2c0772cd89f94
Chalant <Strikerklm96@gmail.com>
2021-07-10 16:46:05 -0500

    Update Exploration1_Colonialism.lua

--- LEKMOD_V28.2/Mods/No Quitters Mod (v 11)/social_policies/8_exploration/Exploration1_Colonialism.lua
+++ LEKMOD_V28.2/Mods/No Quitters Mod (v 11)/social_policies/8_exploration/Exploration1_Colonialism.lua
@@ -1,7 +1,7 @@
 -- Add Governor's Mansion to newly founded cities
 function NQMP_Exploration_Colonialism_OnFoundCity(iPlayer, iCityX, iCityY)
 	local player = Players[iPlayer]
-	if (player:HasPolicy(GameInfo.Policies["POLICY_MERCHANT_NAVY"].ID)) then
+	if (player:HasPolicy(GameInfo.Policies["POLICY_LIBERTY"].ID)) then
 		for loopCity in player:Cities() do
 			if (loopCity:GetX() == iCityX) then
 				if (loopCity:GetY() == iCityY) then
